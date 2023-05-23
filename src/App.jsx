7f873f49409c91115b5727e80306266fa46191d9
bora-sen <bahattinborasen@gmail.com>
2023-05-24 00:01:31 +0300

    +updated Main Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { MainLayout } from "./Layout"
+import * as Section from "./Sections"
 function App() {
-  return <MainLayout>
-    <div>hello world</div>
-  </MainLayout>
+  return (
+    <MainLayout>
+      <Section.Hero />
+      <Section.Featured />
+      <Section.Jackets />
+      <Section.TextBand />
+      <Section.PictureSlide />
+      <Section.TextBand />
+    </MainLayout>
+  )
 }
 
 export default App
